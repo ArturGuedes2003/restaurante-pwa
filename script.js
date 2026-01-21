b25689778f18e96e7678cb99fdb25ad077c13ce6
ArturGuedes2003 <arturwyctor2017@gmail.com>
2026-01-21 16:13:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ console.log("window.supabase =", window.supabase);
 const SUPABASE_URL = "https://dokczdvfyudnhofjphl.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_aYa1L80xClhEU2RIjlPl_A_IffoDTqJ";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-console.log("Supabase conectado:", supabase);
+console.log("sb conectado:", sb);
