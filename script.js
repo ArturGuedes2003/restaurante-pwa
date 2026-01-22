415ed2edba6a3e065271fa1ce6f0396ac2461964
ArturGuedes2003 <arturwyctor2017@gmail.com>
2026-01-22 10:04:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Admin do restaurante rodando!");
 console.log("window.supabase =", window.supabase);
 
-const SUPABASE_URL = "https://dokczdvfyudnhofjphl.supabase.co";
+const SUPABASE_URL = "https://dokcxzdvfyudnhofjphl.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_aYa1L80xClhEU2RIjlPl_A_IffoDTqJ";
 
 const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
